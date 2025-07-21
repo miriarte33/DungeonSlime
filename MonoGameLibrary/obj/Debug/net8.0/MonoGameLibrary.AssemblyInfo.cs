66f10e497feab4771c97346c0ea727e15dc3da33
Manrique Iriarte <m_iriarte33@yahoo.com>
2025-07-20 23:25:57 -0400

    KeyboardInfo class

--- MonoGameLibrary/obj/Debug/net8.0/MonoGameLibrary.AssemblyInfo.cs
+++ MonoGameLibrary/obj/Debug/net8.0/MonoGameLibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MonoGameLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4685919e06dfd10097deb7d221f2d501737460c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3a8d9da83743468c6160871c8df296a9cda7c83")]
 [assembly: System.Reflection.AssemblyProductAttribute("MonoGameLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MonoGameLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
